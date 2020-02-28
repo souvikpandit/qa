aa0b64718c39751f7d041bad33a5016477e3e60e
souvikpandit <souvikpandit3@gmail.com>
2020-02-28 17:16:18 +0530

    created fake answer

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,13 +15,18 @@ class DatabaseSeeder extends Seeder
          * method chaining with each() method
          * create() insert records in database
          * saveMany() creates more than one record
-         * make() will genarate objects and store ir to memory  
+         * make() will genarate objects and store it to memory  
          */
         factory(App\User::class,10)->create()->each( function($user){
             $user->questions()
                 ->saveMany(
                     factory(App\Question::class,rand(1,5))->make()
-                );
+
+                )
+                ->each(function($question){
+                    $question->answers()->saveMany(factory(App\Answer::class,rand(1,5))->make());
+
+                });
         } );
     }
 }
